1f273ea67cb992e45d462dd06eb778210118ceac
krsobo <krsobo@ttu.ee>
2023-03-22 21:56:36 +0200

    Get Average temperature Max and Min and UPDATE-TIME

--- src/main/java/com/example/iot_backend/service/ReportService.java
+++ src/main/java/com/example/iot_backend/service/ReportService.java
@@ -11,6 +11,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
@@ -27,10 +28,11 @@ public class ReportService {
     private float averageMin;
 
     public Report getFullReport() {
+        makeFullReport();
         return report;
     }
 
-    public void makeFullReport(){
+    public void makeFullReport() {
         try {
             ResponseEntity<String> resp = restTemplate.getForEntity(BASE_URL, String.class);
             String res = resp.getBody();
@@ -48,31 +50,65 @@ public class ReportService {
     }
 
     public String getAverageTemperature() {
-        return "MAX-TEMP: "+ String.format("%.2f", averageMax)
-                +", MIN-TEMP: "+ String.format("%.2f", averageMin)
-                + ", LAST-UPDATE: " + getCurrentTimeStamp();
+        return "MAX-TEMP: " + String.format("%.2f", averageMax)
+                + ", MIN-TEMP: " + String.format("%.2f", averageMin)
+                + ", UPDATED: " + getCurrentTimeStamp();
     }
 
-    @Scheduled(fixedRate=60*60*1000)
-    public void countAverageTemperature() {
+    @Scheduled(fixedRate = 60*60*1000)
+    public void recordAverageTemperatureToFile() {
         makeFullReport();
         averageMax = countAverageMax(report.getForecastList().get(0).getDay().getPlaceList());
         averageMin = countAverageMin(report.getForecastList().get(0).getNight().getPlaceList());
+        recordToFile();
     }
+
+    private void recordToFile() {
+        File reportFile = new File("temperatureReport.txt");
+        if (reportFile.exists()) {
+            updateFile(reportFile);
+        } else {
+            createFile(reportFile);
+        }
+    }
+
+    private void updateFile(File reportFile) {
+        try{
+            Writer updatedReport = new BufferedWriter(new FileWriter(reportFile.getName(), true));
+            updatedReport.write(getAverageTemperature()+"\n");
+            updatedReport.close();
+            log.info("Successfully wrote to the updated file.");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private void createFile(File reportFile) {
+        try {
+            FileWriter myWriter = new FileWriter(reportFile.getName());
+            myWriter.write(getAverageTemperature() + "\n");
+            myWriter.close();
+            log.info("Successfully wrote to the file.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 
     private float countAverageMin(List<Place> cities) {
         float sumTemperature = 0;
-        for(Place city: cities){
+        for (Place city : cities) {
             sumTemperature += Integer.parseInt(city.getTempmin());
         }
-        return sumTemperature/cities.size();
+        return sumTemperature / cities.size();
     }
 
     private float countAverageMax(List<Place> cities) {
         float sumTemperature = 0;
-        for(Place city: cities){
+        for (Place city : cities) {
             sumTemperature += Integer.parseInt(city.getTempmax());
         }
-        return sumTemperature/cities.size();
+        return sumTemperature / cities.size();
     }
 }
